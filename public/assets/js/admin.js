0399ac55118a0f3a51d826211e90a18085d46a9a
Razvan Zamfir <razvan.zamfir@ad.isoftbet.com>
2020-03-03 12:13:38 +0200

    remove class 'd-flex' from the tr once it is deleted

--- public/assets/js/admin.js
+++ public/assets/js/admin.js
@@ -1,13 +1,16 @@
 $(document).ready(function(){
   $('.delete-post').on('click', function(evt){
     evt.preventDefault();
-    let postId = $(this).data('id');
+    const postId = $(this).data('id');
+    const $tableRow = $('tr[data-id="' + postId +'"]');
     if(confirm('Delete this post?')) {
       $.ajax({
         url: '/dashboard/post/delete/' + postId,
         method: 'DELETE',
         success: function(){
-          $('tr[data-id="' + postId +'"]').fadeOut('250');
+          $tableRow.fadeOut(250, function() {
+            $tableRow.removeClass('d-flex')
+          });
         }
       });
     }
